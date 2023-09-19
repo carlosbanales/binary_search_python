beb168f75097ccd16be34e384845bf2f2be97eb3
carlosbanales <carlosbanales42@gmail.com>
2023-09-18 22:01:05 -0700

    added comments

--- binary_search.py
+++ binary_search.py
@@ -1,28 +1,31 @@
 import time
 import random
 
-def naive_search(ordered_list, target):
-    for i in range(len(ordered_list)):
-        if ordered_list[i] == target:
+# scans the entire list in order to find the target  
+def naive_search(sorted_list, target):
+    for i in range(len(sorted_list)):
+        if sorted_list[i] == target:
             return i
     return -1
 
-def binary_search(ordered_list, target, low=None, high=None):
+# recursively divide the list until the target is found
+def binary_search(sorted_list, target, low=None, high=None):
     if low is None:
         low = 0
     if high is None: 
-        high = len(ordered_list) - 1
+        high = len(sorted_list) - 1
     if high < low:
         return -1
 
     midpoint = (low + high) // 2
-    if ordered_list[midpoint] == target:
+    if sorted_list[midpoint] == target:
         return midpoint
-    elif target < ordered_list[midpoint]:
-        return binary_search(ordered_list, target, low, midpoint - 1)
+    elif target < sorted_list[midpoint]:
+        return binary_search(sorted_list, target, low, midpoint - 1)
     else:
-        return binary_search(ordered_list, target, midpoint + 1, high)
-    
+        return binary_search(sorted_list, target, midpoint + 1, high)
+
+# main function calls functions and records time to find target
 if __name__=="__main__":
     length = 10000
     sorted_list = set()
