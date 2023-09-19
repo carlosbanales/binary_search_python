62176a1aae9fdcf0ee261d39626e8a81bef62b19
carlosbanales <carlosbanales42@gmail.com>
2023-09-18 20:55:09 -0700

    added main function

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,5 @@
-
+import time
+import random
 
 def naive_search(ordered_list, target):
     for i in range(len(ordered_list)):
@@ -22,3 +23,22 @@ def binary_search(ordered_list, target, low=None, high=None):
     else:
         return binary_search(ordered_list, target, midpoint + 1, high)
     
+if __name__=="__main__":
+    length = 10000
+    sorted_list = set()
+
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time: ", (end-start)/length*1000000,"microseconds")
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end-start)/length*1000000,"microseconds")
